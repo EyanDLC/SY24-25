bb1dda0e7b9068c0d90a47fe143b42688c2a2d82
EyanDLC <ecdel974@learner.dvusd.org>
2024-09-20 12:38:54 -0700

    1st version of game

---
+++ tetris/tetris/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(form1))
+        Avatar = New PictureBox()
+        Timer1 = New Timer(components)
+        CoinPictureBox = New PictureBox()
+        enemy = New PictureBox()
+        Enemy2 = New PictureBox()
+        CType(Avatar, ComponentModel.ISupportInitialize).BeginInit()
+        CType(CoinPictureBox, ComponentModel.ISupportInitialize).BeginInit()
+        CType(enemy, ComponentModel.ISupportInitialize).BeginInit()
+        CType(Enemy2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Avatar
+        ' 
+        Avatar.Image = CType(resources.GetObject("Avatar.Image"), Image)
+        Avatar.Location = New Point(110, 391)
+        Avatar.Name = "Avatar"
+        Avatar.Size = New Size(106, 104)
+        Avatar.SizeMode = PictureBoxSizeMode.Zoom
+        Avatar.TabIndex = 0
+        Avatar.TabStop = False
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Enabled = True
+        ' 
+        ' CoinPictureBox
+        ' 
+        CoinPictureBox.Image = CType(resources.GetObject("CoinPictureBox.Image"), Image)
+        CoinPictureBox.Location = New Point(400, 408)
+        CoinPictureBox.Name = "CoinPictureBox"
+        CoinPictureBox.Size = New Size(125, 62)
+        CoinPictureBox.SizeMode = PictureBoxSizeMode.Zoom
+        CoinPictureBox.TabIndex = 1
+        CoinPictureBox.TabStop = False
+        ' 
+        ' enemy
+        ' 
+        enemy.Image = CType(resources.GetObject("enemy.Image"), Image)
+        enemy.Location = New Point(689, 347)
+        enemy.Name = "enemy"
+        enemy.Size = New Size(272, 194)
+        enemy.SizeMode = PictureBoxSizeMode.Zoom
+        enemy.TabIndex = 2
+        enemy.TabStop = False
+        ' 
+        ' Enemy2
+        ' 
+        Enemy2.Image = CType(resources.GetObject("Enemy2.Image"), Image)
+        Enemy2.Location = New Point(636, 84)
+        Enemy2.Name = "Enemy2"
+        Enemy2.Size = New Size(272, 194)
+        Enemy2.SizeMode = PictureBoxSizeMode.Zoom
+        Enemy2.TabIndex = 3
+        Enemy2.TabStop = False
+        ' 
+        ' form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1030, 622)
+        Controls.Add(Enemy2)
+        Controls.Add(enemy)
+        Controls.Add(CoinPictureBox)
+        Controls.Add(Avatar)
+        Name = "form1"
+        Text = "Form1"
+        CType(Avatar, ComponentModel.ISupportInitialize).EndInit()
+        CType(CoinPictureBox, ComponentModel.ISupportInitialize).EndInit()
+        CType(enemy, ComponentModel.ISupportInitialize).EndInit()
+        CType(Enemy2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Avatar As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents CoinPictureBox As PictureBox
+    Friend WithEvents enemy As PictureBox
+    Friend WithEvents Enemy2 As PictureBox
+
+End Class
